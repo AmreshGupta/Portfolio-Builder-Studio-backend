259c0afcc2b788cdcf8349c39d4e770b69f70e30
AmreshGupta <kumargupta5424@gmail.com>
2026-06-03 09:41:09 +0530

    updated app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(cors({
   },
   credentials: true
 }));
-app.options("*", cors());
+
 
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
